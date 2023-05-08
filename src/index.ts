f1f6850c2639606bf8194b5f42bf6c4141ffddbe
Alex // timetocode <alex@timetocode.com>
2023-05-09 02:00:21 +0300

    no more json parse

--- src/index.ts
+++ src/index.ts
@@ -44,7 +44,7 @@ class WebSocketClientAdapter {
         }
 
         socket.onclose = (event) => {
-            this.network.onDisconnect(JSON.parse(event.reason), event)
+            this.network.onDisconnect(event.reason, event)
         }
 
         socket.onerror = (event) => {
